8552582c23a8b80ca9af9b900a8b1358a9112b00
Rafael Rezende <rafael.yanase@gmail.com>
2025-10-15 10:46:52 +0100

    Update: refactor of helper functions to simplify the operator handling logic.

--- src/parser/ft_lexer.c
+++ src/parser/ft_lexer.c
@@ -47,73 +47,74 @@ t_token	*ft_handle_operator(t_lexer *l)
     current = l->input[l->pos];
     if (current == '|')
 	ft_which_or(l);
-    else if (*ptr == '&')
+    else if (current == '&')
 	ft_which_and(l);
-    else if (*ptr == '<' || *ptr == '>')
+    else if (current == '<' || current == '>')
 	ft_which_redir(l);
-    else if (*ptr == '(')
+    else if (current == '(')
     {
 	l->pos++;
-	return (create_token(TOKEN_LEFT_PAR, "("));
+	return (create_token(TOKEN_LEFT_PAR, ft_strdup("(")));
     }
-    else if (*ptr == ')')
+    else if (current == ')')
     {
 	l->pos++;
-	return (create_token(TOKEN_RIGHT_PAR, ")"));
+	return (create_token(TOKEN_RIGHT_PAR, ft_strdup(")")));
     }
     else
-	return (create_token(TOKEN_ERROR, "Unsuported operator"));
+	return (create_token(TOKEN_ERROR, ft_strdup("Unsuported operator")));
 }
 
-void	ft_which_or(t_lexer *l)
+t_token	*ft_which_or(t_lexer *l)
 {
-    if (l->pos + 1 < l->len && *(ptr + 1) == '|')
+    if (l->pos + 1 < l->len && l->input[l->pos + 1] == '|')
     {
-	tok = create_token(TOKEN_OR, "||");
 	l->pos += 2;
+	return (create_token(TOKEN_OR, ft_strdup("||")));
     }
     else
     {
-	tok = create_token(TOKEN_PIPE, "|");
 	l->pos++;
+	return (create_token(TOKEN_PIPE, ft_strdup("|")));
     }
 }
 
-void	ft_which_and(t_lexer *l)
+t_token	*ft_which_and(t_lexer *l)
 {
-    if (l->pos + 1 < l->len && *(ptr + 1) == '&')
+    if (l->pos + 1 < l->len && l->input[l->pos + 1] == '&')
     {
-	tok = create_token(TOKEN_AND, "&&");
 	l->pos += 2;
+	return (create_token(TOKEN_AND, ft_strdup("&&")));
     }
+    return (create_token(TOKEN_ERROR, ft_strdup("Unsuported operator")));
 }
 
-void	ft_which_redir(t_lexer *l)
+t_token	*ft_which_redir(t_lexer *l)
 {
     char	*delim;
 
-    if (l->pos + 1 < l->len && *(ptr + 1) == '>')
+    if (l->pos + 1 < l->len && l->input[l->pos + 1] == '>')
     {
 	l->pos += 2;
-	return create_token(TOKEN_REDIR_APPEND, ">>"));
+	return create_token(TOKEN_REDIR_APPEND, ft_strdup(">>")));
     }
-    else if (l->pos + 1 < l->len && *(ptr + 1) == '<')
+    else if (l->pos + 1 < l->len && l->input[l->pos + 1] == '<')
     {
 	l->pos += 2;
 	while (l->pos < l->len && ft_isspace(l->input[l->pos]))
 	    l->pos++;
-	delim = ft_get_unquoted_str(l->input[l->pos]);
+	delim = ft_get_unquoted_str(l);
 	if (!delim)
-	    tok = create_token(TOKEN_ERROR, "minishell: syntax error near unexpected token `<'");
-	tok = create_token(TOKEN_REDIR_HEREDOC, delim);
+	    return (create_token(TOKEN_ERROR, ft_strdup("minishell: syntax error near unexpected token `<'")));
+	return (create_token(TOKEN_REDIR_HEREDOC, delim));
     }
     else if (*ptr == "<")
     {
-	tok = create_token(TOKEN_REDIR_IN, "<");
 	l->pos++;
+	return (create_token(TOKEN_REDIR_IN, ft_strdup("<")));
     }
     else
-	return ;
+	return (create_token(TOKEN_ERROR, ft_strdup("minishell: syntax error near unexpected token `<'")));
 }
 /*
 t_token	*ft_handle_word()
