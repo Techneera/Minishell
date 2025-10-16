75f86c645a47178ba7d472931b3ffe96a7f83340
Rafael Rezende <rafael.yanase@gmail.com>
2025-10-16 15:06:17 +0100

    Fix: missing IN operator.

--- src/parser/ft_lexer.c
+++ src/parser/ft_lexer.c
@@ -21,20 +21,13 @@ t_lexer	*ft_state_lexer(const char *line)
 
 t_token	*get_next_token(t_lexer *l)
 {
-    char	*ptr;
-
-    ptr = NULL;
     if (!l)
 	return (NULL);
-    ptr = l->input + l->pos;
-    while (*ptr && ft_isspace(*ptr))
-    {
+    while (l->input[l->pos] && ft_isspace(l->input[l->pos]))
 	l->pos++;
-	ptr++;
-    }
     if (l->pos >= l->len)
 	return (create_token(TOKEN_EOF, NULL));
-    if (ft_ismeta(*ptr))
+    if (ft_ismeta(l->input[l->pos]))
 	return (ft_handle_operator(l));
     else
 	return (ft_handle_word(l));
@@ -46,11 +39,11 @@ t_token	*ft_handle_operator(t_lexer *l)
 
     current = l->input[l->pos];
     if (current == '|')
-	ft_which_or(l);
+	return (ft_which_or(l));
     else if (current == '&')
-	ft_which_and(l);
+	return (ft_which_and(l));
     else if (current == '<' || current == '>')
-	ft_which_redir(l);
+	return (ft_which_redir(l));
     else if (current == '(')
     {
 	l->pos++;
@@ -86,6 +79,7 @@ t_token	*ft_which_and(t_lexer *l)
 	l->pos += 2;
 	return (create_token(TOKEN_AND, ft_strdup("&&")));
     }
+    l->pos++;
     return (create_token(TOKEN_ERROR, ft_strdup("Unsuported operator")));
 }
 
@@ -96,7 +90,12 @@ t_token	*ft_which_redir(t_lexer *l)
     if (l->pos + 1 < l->len && l->input[l->pos + 1] == '>')
     {
 	l->pos += 2;
-	return create_token(TOKEN_REDIR_APPEND, ft_strdup(">>")));
+	return (create_token(TOKEN_REDIR_APPEND, ft_strdup(">>")));
+    }
+    else if (l->input[l->pos] == '>')
+    {
+	l->pos++;
+	return (create_token(TOKEN_REDIR_OUT, ft_strdup("<")));
     }
     else if (l->pos + 1 < l->len && l->input[l->pos + 1] == '<')
     {
@@ -105,10 +104,10 @@ t_token	*ft_which_redir(t_lexer *l)
 	    l->pos++;
 	delim = ft_get_unquoted_str(l);
 	if (!delim)
-	    return (create_token(TOKEN_ERROR, ft_strdup("minishell: syntax error near unexpected token `<'")));
+	    return (create_token(TOKEN_ERROR, ft_strdup("Unexpected error in delim.")));
 	return (create_token(TOKEN_REDIR_HEREDOC, delim));
     }
-    else if (*ptr == "<")
+    else if (l->input[l->pos] == '<')
     {
 	l->pos++;
 	return (create_token(TOKEN_REDIR_IN, ft_strdup("<")));
