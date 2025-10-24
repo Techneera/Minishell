00ba46f90e430196f6c9339b01f67a082a24e63f
Rafael Rezende <rafael.yanase@gmail.com>
2025-10-24 17:58:38 +0100

    Update: created token list constructor function, created additional fields to ast structure.

--- include/ast.h
+++ include/ast.h
@@ -7,11 +7,14 @@ typedef struct s_parser
 {
 	t_lexer	*lex;
 	t_token	*next_token;
-	t_token	*peak;
-}
+	t_token	*peek;
+}	t_parser;
 
-t_ast	*ft_ast_node(t_cmd *cmd);
-t_cmd	*ft_create_cmd(char **av, t_redir *redirs);
+t_ast	*ft_ast_node_command(t_cmd *cmd);
+t_ast	*ft_ast_generic_node(t_node_type type);
+t_ast	*ft_init_parser(t_lexer *l);
+t_ast	*ft_parse_input(t_lexer *l);
+t_cmd	*ft_create_command(char **av, t_redir *redirs, int count);
 t_redir	*ft_create_redir(t_label_redir label, char *str);
 void	ft_free_cmd(t_cmd *cmd);
 void	ft_free_node(t_ast *root);
