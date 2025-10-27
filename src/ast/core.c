0319481588d2a6cdfd1c19b76d3f7d46f80cb190
Rafael Luis Yanase de Rezende <rafael.yanase@gmail.com>
2025-10-27 23:22:11 +0000

    Update: add helper functions of parse simple command.

--- src/ast/core.c
+++ src/ast/core.c
@@ -45,17 +45,10 @@ t_ast	*ft_parse_and_or(t_parser *parser)
 		ft_parser_iter(parser);
 		ast_right = ft_parse_pipeline(parser);
 		if (!ast_right)
-		{
-			ft_free_ast(ast_left);
-			return (NULL);
-		}
+			return (ft_free_ast(ast_left), NULL);
 		new_parent = ft_ast_generic_node(op_type);
 		if (!new_parent)
-		{
-			ft_free_ast(ast_left);
-			ft_free_ast(ast_right);
-			return (NULL);
-		}
+			return (ft_free_ast(ast_left), ft_free_ast(ast_right), NULL);
 		new_parent->left = ast_left;
 		new_parent->right= ast_right;
 		ast_left = new_parent;
@@ -87,9 +80,82 @@ t_ast	*ft_parse_pipeline(t_parser *parser)
 	}
 	return (ast_left);
 }
-/*
+
 t_ast	*ft_parse_grain_with_redirs(t_parser *parser)
 {
-	t_ast	*
+	t_ast	*node;
+
+	node = NULL;
+	if (parser->current_token->tok_label == TOKEN_LEFT_PAR)
+		node = ft_parse_subshell(parser);
+	else if (parser->current_token->tok_label == TOKEN_WORD)
+		node = ft_parse_node_command(parser);
+	else
+		fprintf(stderr, "Error invalid token.");
+	if (!node)
+		return (NULL);
+	ft_handle_redirects(parser, &node);
+}
+
+t_ast	*ft_parse_node_command(t_parser *parser)
+{
+	t_ast	*node_cmd;
+	t_cmd	*cmd;
+
+	cmd = ft_create_command(NULL, NULL, 0);
+	if (!cmd)
+		return (NULL);
+	cmd->args = ft_parse_args(parser);
+	if (!cmd->args)
+		return (free(cmd), NULL);
+	node_cmd = ft_ast_node_command(cmd);
+	return (node_cmd);
+}
+
+char	**ft_parse_args(t_parser *parser)
+{
+	t_list	*head;
+	char	**args_ret;
+	char	*arg_cpy;
+
+	head = NULL;
+	while (parser->current_token->tok_label == TOKEN_WORD)
+	{
+		arg_cpy = strdup(parser->current_token->str);
+		if (!arg_cpy)
+			return (ft_lstclear(&head, &free), NULL);
+		ft_lstadd_back(&head, ft_lstnew(arg_cpy));
+		ft_parser_iter(parser);
+	}
+	args_ret = ft_lst_to_args(&head, ft_lstsize(head));
+	if (!args_ret)
+		return (ft_lstclear(&head, &free), NULL);
+	return (args_ret);
+}
+
+char	**ft_lst_to_args(t_lst **head, int size)
+{
+	char	**ret;
+	t_lst	*tmp;
+	int		i;
+
+	ret = (char **)ft_calloc(size + 1, sizeof(char *));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (*head)
+	{
+		tmp = *head;
+		ret[i] = (char *)tmp->content;
+		*head = tmp->next;
+		free(tmp);
+		i++;
+	}
+	return (ret);
+}
+/*
+t_ast	*ft_parse_subshell(parser)
+{
+	
 }
 */
