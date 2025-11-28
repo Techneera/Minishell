7da16bb16593d5a860cd3732bf7a1b109fe506ca
Nicholas Saraiva <nicholas.saraivadev@gmail.com>
2025-11-28 15:11:24 +0000

    UPDATE: Verification if exist or not to insert on list in export

--- src/execution/ft_export.c
+++ src/execution/ft_export.c
@@ -1,12 +1,14 @@
 #include "execution.h"
 
 static int	has_arg(char *arg);
+void	insert_in_list(t_list *list, char *arg);
+int		insert_if_exist(t_list *list, char *arg);
+int		is_valid(char *arg);
 
 void	ft_export(t_list *list, t_ast *node)
 {
 	int		n_args;
 	int 	i;
-	t_list 	*new_arg;
 
 	i = 0;
 	if (!list || !node)
@@ -18,13 +20,80 @@ void	ft_export(t_list *list, t_ast *node)
 	{
 		while (++i < n_args)
 		{
-			new_arg = create_node_env(node->cmd->args[i], has_arg(node->cmd->args[i]));
-			if (!new_arg)
-				return ;
-			ft_lstadd_back(&list, new_arg);
+			if (is_valid(node->cmd->args[i]))
+				insert_in_list(list, node->cmd->args[i]);
+			else
+			{
+				ft_putstr_fd("minishell: export: `", 2);
+				ft_putstr_fd(node->cmd->args[i], 2);
+				ft_putstr_fd("': not a valid identifier\n", 2);
+			}
 		}
-		ft_print_sorted_export(list);
 	}
+}
+
+int	is_valid(char *arg)
+{
+	int	i;
+
+	i = 0;
+	if (!arg)
+		return (0);
+	if (!ft_isalpha(arg[0]))
+		return (0);
+	while (ft_isalpha(arg[i]) || ft_isalnum(arg[i]))
+		i++;
+	if (!arg[i])
+		return (1);
+	if (arg[i] == '=')
+		return (1);
+	return (0);
+}
+
+void	insert_in_list(t_list *list, char *arg)
+{
+	t_list	*actual;
+	t_list 	*new_arg;
+
+	actual = list;
+	if (!arg)
+		return ;
+	if (insert_if_exist(list, arg))
+		return ;
+	new_arg = create_node_env(arg, has_arg(arg));
+	if (!new_arg)
+		return ;
+	ft_lstadd_back(&list, new_arg);
+	return ;
+}
+
+int	insert_if_exist(t_list *list, char *arg)
+{
+	t_env	*env;
+	t_list	*actual;
+	int		i;
+
+	i = 0;
+	actual = list;
+	while (actual)
+	{
+		i = 0;
+		env = (t_env *) actual->content;
+		if (!env->variable || !arg)
+			return (0);
+		while (env->variable[i] && arg[i] && env->variable[i] != '=' && env->variable[i] == arg[i])
+			i++;
+		if ((!env->variable[i] && !arg[i]) || (env->variable[i] == '='))
+		{
+			free (env->variable);
+			env->variable = ft_strdup(arg);
+			if (!env->variable)
+				perror("failed calloc on ft_strdup on export");
+			return (1);
+		}
+		actual = actual->next;
+	}
+	return (0);
 }
 
 static int	has_arg(char *arg)
