3d87a289b34a82e9a08d35072b70ddf8cb7d27be
Rafael Rezende <rafael.yanase@gmail.com>
2025-12-01 08:57:09 +0000

    Update: transform envp linked list back into array of arrays.

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -77,6 +77,34 @@ static void	remove_env_var(t_data *data, char *key)
 	}
 }
 
+static
+void	refresh_env_array(t_data *data)
+{
+	int		i;
+	int		len;
+	t_list	*curr;
+	t_env	*content;
+
+	if (data->envp)
+		ft_free_array(data->envp);
+	len = ft_lstsize(data->env_list);
+	data->envp = malloc(sizeof(char *) * (len + 1));
+	if (!data->envp)
+		failed_malloc(data, "envp update");
+	curr = data->env_list;
+	i = 0;
+	while (curr)
+	{
+		content = (t_env *)curr->content;
+		data->envp[i] = ft_strdup(content->variable);
+		if (!data->envp[i])
+			failed_malloc(data, "envp duplication");
+		i++;
+		curr = curr->next;
+	}
+	data->envp[i] = NULL;
+}
+
 int	ft_unset(t_data *data)
 {
 	int		i;
@@ -101,5 +129,7 @@ int	ft_unset(t_data *data)
 			remove_env_var(data, args[i]);
 		i++;
 	}
+	if (ret == 0 || i > 1)
+		refresh_env_array(data);
 	return (ret);
 }
